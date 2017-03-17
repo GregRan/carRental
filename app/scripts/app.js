713b2a7eff7cafd1537462efd09056cf6678f219
GuoRan <374579168@qq.com>
2017-03-17 16:01:40 +0800

    1.02

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -18,4 +18,44 @@ angular
   			url:"/login",
   			templateUrl:"views/login.html"
   		})
+  		$stateProvider.state("staffHomepage",{
+  			url:"/staffHomepage",
+  			templateUrl:"views/staffHomepage.html"
+  		})
+  		$stateProvider.state("carList",{
+  			url:"/carList",
+  			templateUrl:"views/carList.html"
+  		})
+  		$stateProvider.state("carDetails",{
+  			url:"/carDetails",
+  			templateUrl:"views/carDetails.html"
+  		})
+  		$stateProvider.state("noticeList",{
+  			url:"/noticeList",
+  			templateUrl:"views/noticeList.html"
+  		})
+  		$stateProvider.state("noticeDetails",{
+  			url:"/noticeDetails",
+  			templateUrl:"views/noticeDetails.html"
+  		})
+  		$stateProvider.state("personal",{
+  			url:"/personal",
+  			templateUrl:"views/personal.html"
+  		})
+  		$stateProvider.state("bossHomepage",{
+  			url:"/bossHomepage",
+  			templateUrl:"views/bossHomepage.html"
+  		})
+  		$stateProvider.state("staffList",{
+  			url:"/staffList",
+  			templateUrl:"views/staffList.html"
+  		})
+  		$stateProvider.state("release",{
+  			url:"/release",
+  			templateUrl:"views/release.html"
+  		})
+  		$stateProvider.state("addCar",{
+  			url:"/addCar",
+  			templateUrl:"views/addCar.html"
+  		})
   }]);
