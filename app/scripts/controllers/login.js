f3b13e3094906cef5852ca267a5429cbb992b502
GuoRan <374579168@qq.com>
2017-03-20 23:19:42 +0800

    1.03

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -7,7 +7,45 @@
  * # MainCtrl
  * Controller of the appApp
  */
-angular.module('appApp')
-  .controller('loginCtrl', function ($scope) {
-    	$scope.aa="123"
-  });
+angular.module('appApp').controller('loginCtrl',["$scope","$http","$state",function ($scope,$http,$state){
+	$scope.updata={
+		userName:"",passWord:""
+	}
+	$scope.nameReg = /^1[3,4,5,7,8]\d{9}$/;
+	$scope.passReg = /^(?!\d+$)(?![A-Za-z]+$)[a-zA-Z0-9{6,}$]/;
+  	$scope.blur=function(){
+  		if(!$scope.nameReg.test($scope.updata.userName)){
+  			$('.modal').modal('show')
+  		}
+  	}
+  	$scope.blur2=function(){
+  		if(!$scope.passReg.test($scope.updata.passWord)){
+  			$('.modal').modal('show')
+  		}
+  	}
+    $scope.login=function(){
+    	if(!$scope.nameReg.test($scope.updata.userName)||!$scope.passReg.test($scope.updata.passWord)){
+  			$('.modal').modal('show')
+  		}else {
+	    	$http({
+		    	url:"http://47.88.16.225:407/users/login",
+		    	method:"post",
+		    	data:{"username":$scope.updata.userName,"password":$scope.updata.passWord}
+		    }).then(function(e){
+		    	localStorage.setItem("uid",e.data.uid);
+		    	$http({
+			    	url:"http://47.88.16.225:407/users/"+localStorage.uid,
+			    	method:"get",
+			    }).then(function(event){
+					if(event.data.level=="0"){
+						$state.go("staffHomepage");
+					}else{
+						$state.go("bossHomepage");
+					}
+			    })
+		    },function(e){
+		    	$('.modal').modal('show')
+		    })
+	   	}
+    }
+}]);
