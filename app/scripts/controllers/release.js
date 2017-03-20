8dc06f12e7e9c4ad2ffea9c07f5d9c556868c85d
mm <811198280@qq.com>
2017-03-20 15:45:19 +0800

    1.0.2发布公告

--- app/scripts/controllers/release.js
+++ app/scripts/controllers/release.js
@@ -8,6 +8,24 @@
  * Controller of the appApp
  */
 angular.module('appApp')
-  .controller('releaseCtrl', function ($scope) {
-//  	$scope.aa="123"
-  });
+	.controller('releaseCtrl', ['$scope', '$http', function($scope, $http) {
+		$scope.Mtext = "";
+		$scope.btn = function() {
+			if($scope.Mtext != "") {
+				$http({
+					method: 'post',
+					url: 'http://47.88.16.225:407/gonggao',
+					data: {
+						"name": $scope.Mtext,
+						"time": new Date()
+					}
+				}).then(function(e) {
+					console.log(e.data)
+					$scope.Mtext = ''
+				})
+				$("#layer").modal('show')
+			} else {
+				$("#layer").modal('hide')
+			}
+		}
+	}]);
