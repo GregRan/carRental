e56f106284464982a2cb8ee2ba698ab605529396
rjx <761481541@qq.com>
2017-03-21 19:18:07 +0800

    2017.3.21晚

--- app/scripts/controllers/addStaff.js
+++ app/scripts/controllers/addStaff.js
@@ -8,6 +8,18 @@
  * Controller of the appApp
  */
 angular.module('appApp')
-  .controller('addStaffCtrl', function ($scope) {
-    	$scope.aa="123"
-  });
+  .controller('addStaffCtrl',['$scope','$state','$http',function ($scope,$state,$http) {
+    	$scope.btn=function(){
+    		$state.go('staffList')
+    	}
+    	$scope.obj={};
+    	$scope.btn1=function(){
+    		$http({
+    			method:"post",
+    			url:"http://47.88.16.225:407/users",
+    			data:$scope.obj
+    		}).then(function(e){
+    			$state.go('staffList')
+    		})	
+    	}
+  }]);
