368f75cae9b08c602bd346950f759c2aee4e3785
mm <811198280@qq.com>
2017-03-22 16:46:07 +0800

    1.05

--- app/scripts/controllers/carList.js
+++ app/scripts/controllers/carList.js
@@ -9,14 +9,38 @@
  */
 angular.module('appApp')
   .controller('listCtrl',['$scope','$http','$state','$filter', function ($scope,$http,$state,$filter) {
-    	$http({
-    		method:'get',
-    		url:'http://47.88.16.225:407/car',
-    	}).then(function(e){
-    		console.log(e)
-    		$scope.data = e.data
-    		console.log($scope.data)
-    	})
+    	$scope.play=function(num,step){
+    		$http({
+	    		method:'get',
+	    		url:'http://47.88.16.225:407/car?{"$skip":'+num+',"$limit":'+step+'}',
+	    	}).then(function(e){
+	    		$scope.data = e.data
+	    	})
+    	}
+    	$scope.play(0,3)
+    	$scope.shuai=0;
+    	$scope.ss=0;
+    	$scope.next=function(){
+    		$scope.shuai+=3;
+    		$scope.ss++;
+    		document.querySelector(".prev").removeAttribute("disabled")
+    		if($scope.data.length<3){
+    			$scope.shuai=($scope.ss-1)*3;
+    			document.querySelector(".next").setAttribute("disabled","disabled")
+    		}
+    		$scope.play($scope.shuai,3)
+    	}
+    	$scope.prev=function(){
+    		$scope.shuai-=3;
+    		document.querySelector(".next").removeAttribute("disabled")
+    		if($scope.shuai<0){
+    			$scope.shuai=0;
+    			document.querySelector(".prev").setAttribute("disabled","disabled")
+    		}else{
+    			$scope.play($scope.shuai,-3)
+    		}
+    		
+    	}
     	$scope.details = function(car){
     		localStorage.setItem('id',car.id);
     		$state.go('carDetails');
