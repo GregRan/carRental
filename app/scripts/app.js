f5f378ccf6b07c72a75855645c0dd3d8cd726588
gaobo <517551810@qq.com>
2017-03-26 19:11:28 +0800

    gggg

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -62,4 +62,13 @@ angular
   			url:"/addStaff",
   			templateUrl:"views/addStaff.html"
   		})
+  		$stateProvider.state("carList.search",{
+  			url:"/search",
+  			views:{
+  				content:{
+  					templateUrl:"views/search.html"
+  				}
+  			}
+  			
+  		})
   }]);
