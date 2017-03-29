4733e6359f5c39d941bcc39404f59fef8baea908
mm <811198280@qq.com>
2017-03-29 15:21:06 +0800

    2.0.9

--- app/scripts/controllers/noticeList.js
+++ app/scripts/controllers/noticeList.js
@@ -12,67 +12,74 @@ angular.module('appApp')
 		if(sessionStorage.user){
 			$scope.staffList_jiazai=true;
 			$http({
-			method:"get",
-			url:urlId+"/gonggao"
-		}).then(function(e){
-			console.log(e.data)
-    		$scope.staffList_jiazai=false;
-    		$scope.staffList_content=true;
-			$scope.data=e.data;
-			$scope.sub=function(t){
-				sessionStorage.setItem("noticeList_id",t.id)
-				$state.go('noticeDetails')
-			}
-		})
-		
-		$scope.fan=function(){
-			if(sessionStorage.level==0){
-				$state.go('staffHomepage')
-			}else{
-				$state.go('bossHomepage')
-			}
-  		}
-		$scope.index=0;
-		$scope.del_notice=function(ev,e){
-			ev.stopPropagation();
-			$(".modal").modal('show');
-			$scope.index=e;
-		}
-		$scope.n_delete=function(){
-			$http({
-				url:urlId+"/gonggao/"+sessionStorage.noticeList_id,
-				method:"delete"
-			}).then(function(){
-				$(".Mtext").eq($scope.index).remove()
+				method:"get",
+				url:urlId+"/gonggao/?read=0"
+			}).then(function(e){
+				console.log(e.data)
+	    		$scope.staffList_jiazai=false;
+	    		$scope.staffList_content=true;
+				$scope.data=e.data;
 			})
-		}
+			$http({
+				method:"get",
+				url:urlId+"/gonggao/?read=1"
+			}).then(function(e){
+				console.log(e.data)
+	    		$scope.staffList_jiazai=false;
+	    		$scope.staffList_content=true;
+				$scope.dataa=e.data;
+			})
+			$scope.sub=function(t){
+					sessionStorage.setItem("noticeList_id",t.id)
+					$state.go('noticeDetails')
+				}
+			$scope.fan=function(){
+				if(sessionStorage.level==0){
+					$state.go('staffHomepage')
+				}else{
+					$state.go('bossHomepage')
+				}
+	  		}
+			$scope.index=0;
+			$scope.del_notice=function(ev,e){
+				ev.stopPropagation();
+				$(".modal").modal('show');
+				$scope.index=e;
+			}
+			$scope.n_delete=function(){
+				$http({
+					url:urlId+"/gonggao/"+sessionStorage.noticeList_id,
+					method:"delete"
+				}).then(function(){
+					$(".Mtext").eq($scope.index).remove()
+				})
+			}
 		}else{
 			$state.go("login");
 		}
 		
-	}]).directive("setDel",function(){
-				return function(scope,element,attrs){
-						scope.a=0;
-						scope.b=[];
-						scope.c=0;
-						element.on("touchstart",function(e){
-							sessionStorage.setItem("noticeList_id",attrs.setDel)
-							scope.a=e.touches[0].clientX;
-							element.css("transition","0.5s")
-						})
-						element.on("touchmove",function(e){
-							scope.b.push(e.touches[0].clientX);
-							element.css("transition","0.5s")
-						})
-						element.on("touchend",function(e){
-							scope.c=scope.b[scope.b.length-1]-scope.a;
-							if(scope.c<0&&scope.c<-100){
-								element.css("transition","0.5s").css("right","3rem")
-							}else if(scope.c>0&&scope.c>100){
-								element.css("transition","0.5s").css("right",0)
-							}
-						})
-						
-					
+	}])
+	.directive("setDel",function(){
+		return function(scope,element,attrs){
+			scope.a=0;
+			scope.b=[];
+			scope.c=0;
+			element.on("touchstart",function(e){
+				sessionStorage.setItem("noticeList_id",attrs.setDel)
+				scope.a=e.touches[0].clientX;
+				element.css("transition","0.5s")
+			})
+			element.on("touchmove",function(e){
+				scope.b.push(e.touches[0].clientX);
+				element.css("transition","0.5s")
+			})
+			element.on("touchend",function(e){
+				scope.c=scope.b[scope.b.length-1]-scope.a;
+				if(scope.c<0&&scope.c<-100){
+					element.css("transition","0.5s").css("right","3rem")
+				}else if(scope.c>0&&scope.c>100){
+					element.css("transition","0.5s").css("right",0)
 				}
-			});
+			})
+		}
+	});
