c9de43ea70e7af25c342ab154d0c0909dbabdc2f
gaobo <517551810@qq.com>
2017-03-29 11:52:26 +0800

    bobo

--- app/scripts/controllers/addCar.js
+++ app/scripts/controllers/addCar.js
@@ -50,6 +50,77 @@ angular.module('appApp')
 	    	$scope.addCar_back = function(){
 	    		$state.go('bossHomepage')
 	    	}
+	    	$scope.text_align_left = function(){
+	    		$('.add_cm').css('text-align','left');
+	    	}
+	    	$scope.text_align_right = function(){
+	    		$('.add_cm').css('text-align','right');
+	    	}
+	    	$scope.left = function(){
+	    		$('.add_pp').css('text-align','left');
+	    	}
+	    	$scope.right = function(){
+	    		$('.add_pp').css('text-align','right');
+	    	}
+	    	$scope.left_one = function(){
+	    		$('.add_jg').css('text-align','left');
+	    	}
+	    	$scope.right_one = function(){
+	    		$('.add_jg').css('text-align','right');
+	    	}
+	    	$scope.left_two = function(){
+	    		$('.add_cj').css('text-align','left');
+	    	}
+	    	$scope.right_two = function(){
+	    		$('.add_cj').css('text-align','right');
+	    	}
+	    	$scope.left_three = function(){
+	    		$('.add_ys').css('text-align','left');
+	    	}
+	    	$scope.right_three = function(){
+	    		$('.add_ys').css('text-align','right');
+	    	}
+	    	$scope.left_fouth = function(){
+	    		$('.add_lc').css('text-align','left');
+	    	}
+	    	$scope.right_fouth = function(){
+	    		$('.add_lc').css('text-align','right');
+	    	}
+	    	$scope.left_five = function(){
+	    		$('.add_pl').css('text-align','left');
+	    	}
+	    	$scope.right_five = function(){
+	    		$('.add_pl').css('text-align','right');
+	    	}
+	    	$scope.left_six = function(){
+	    		$('.add_cs').css('text-align','left');
+	    	}
+	    	$scope.right_six = function(){
+	    		$('.add_cs').css('text-align','right');
+	    	}
+	    	$scope.left_seven = function(){
+	    		$('.add_ckg').css('text-align','left');
+	    	}
+	    	$scope.right_seven = function(){
+	    		$('.add_ckg').css('text-align','right');
+	    	}
+	    	$scope.left_eight = function(){
+	    		$('.add_rj').css('text-align','left');
+	    	}
+	    	$scope.right_eight = function(){
+	    		$('.add_rj').css('text-align','right');
+	    	}
+	    	$scope.left_nine = function(){
+	    		$('.add_rl').css('text-align','left');
+	    	}
+	    	$scope.right_nine = function(){
+	    		$('.add_rl').css('text-align','right');
+	    	}
+	    	$(".pickdate").dateDropper({
+				animate: false,
+				format: 'Y-m-d',
+				maxYear: '2020'
+		});
   		}else{
   			$state.go("login");
   		}
